47b3b233a80261d04b5b34500875f38cf7d8e5e2
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2022-11-22 21:51:46 +0530

    add schema

--- postgres/02_metabase_seed_db.sh
+++ postgres/02_metabase_seed_db.sh
@@ -7,11 +7,10 @@ POSTGRES="psql -U postgres"
 # create database for superset
 echo "Seeding database: metabase"
 $POSTGRES <<EOSQL
-CREATE TABLE pg_catalog.users (
-  user_id int PRIMARY KEY,
+CREATE SCHEMA test_schema
+    CREATE TABLE test_users (  user_id int PRIMARY KEY,
   username varchar(25) NOT NULL,
-  country varchar(30) NOT NULL
-);
-INSERT into users VALUES (1, "NN", "IN"), (2, "BN", "NZ"), (3, "SS", "AU"), (4, "GL", "RUS");
+  country varchar(30) NOT NULL);
+INSERT into test_users VALUES (1, "NN", "IN"), (2, "BN", "NZ"), (3, "SS", "AU"), (4, "GL", "RUS");
 SELECT * FROM users;
 EOSQL
