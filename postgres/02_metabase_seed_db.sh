1dc43627fed383d91f499fb18b1bdd7503f45084
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2022-11-22 21:17:43 +0530

    seed db

--- postgres/02_metabase_seed_db.sh
+++ postgres/02_metabase_seed_db.sh
@@ -7,11 +7,12 @@ POSTGRES="psql -U postgres"
 # create database for superset
 echo "Seeding database: metabase"
 $POSTGRES <<EOSQL
-CREATE TABLE postgres.users (
+USE postgres
+CREATE TABLE users (
   user_id int PRIMARY KEY,
   username varchar(25) NOT NULL,
   country varchar(30) NOT NULL
 );
-INSERT into users VALUES (1, NN, IN), (2, BN, NZ), (3, SS, AU), (4, G, RUS);
+INSERT into users VALUES (1, "NN", "IN"), (2, "BN", "NZ"), (3, "SS", "AU"), (4, "G", "RUS");
 SELECT * FROM users;
 EOSQL
