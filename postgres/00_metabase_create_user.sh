b9ad53d641193e23d057343e34c9bd8bd087a334
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2022-11-22 16:50:40 +0530

    create user

--- postgres/00_metabase_create_user.sh
+++ postgres/00_metabase_create_user.sh
@@ -2,7 +2,7 @@
 
 set -e # exit if a command exits with a not-zero exit code
 
-POSTGRES="psql -U postgres"
+POSTGRES="sudo -u postgres createuser postgres"
 
 # create a shared role to read & write general datasets into postgres
 echo "Creating database role: metabase"
